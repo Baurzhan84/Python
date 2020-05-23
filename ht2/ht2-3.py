981bb76487806a6514e5189f48776e56da6d8cbe
baurzhan <bbbexeitov@mail.ru>
2020-05-23 10:41:49 +0600

    homework for lessons 2

---
+++ ht2/ht2-3.py
@@ -0,0 +1,11 @@
+# Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+number_month = int(input('number of month:  '))
+if number_month == season
+    season_list = [[12, 1, 2, 'winter'], [3, 4, 5, 'spring'], [6, 7, 8, 'summer'],
+                   [9, 10, 10, 'fall']]
+season = {}
+for i in season_list:
+    season[i[0]] = i[1]
+print(season)
